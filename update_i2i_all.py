3eab5489569b7bdb77294b8a77d226b3504c42ec
Neerav Karani <nkarani@ethz.ch>
2020-07-16 13:36:52 +0200

    Update update_i2i_all.py

--- update_i2i_all.py
+++ update_i2i_all.py
@@ -1,6 +1,5 @@
 import subprocess
 import config.system as sys_config
 
-for subject_id in range(1, 2):
-    # subprocess.call(['python', '/usr/bmicnas01/data-biwi-01/nkarani/projects/dg_seg/methods/tto_ss/tmp/update_i2i.py', str(subject_id)])
+for subject_id in range(2): # num_subjects
     subprocess.call(['python', sys_config.project_code_root + 'update_i2i.py', str(subject_id)])
